6049a788dab5b44b88bd373f4a4aca6e77a0c0d2
JBellows-git <jbellows212@gmail.com>
2021-12-16 00:37:46 -0600

    Starting to add Classes and other important stuff

---
+++ CapStoneAdventure.cs
@@ -0,0 +1,36 @@
+﻿using CSAEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace CapStoneAdventure
+{
+    public partial class CapStoneAdventure : Form
+    {
+        private Player _player;
+        public CapStoneAdventure()
+        {
+            Location location = new Location(1, "Home", "This is your house.");
+            
+            InitializeComponent();
+
+            _player = new Player(10,10,20,0,1);            
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace CapStoneAdventure
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CapStoneAdventure());
         }
     }
 }
